b6126cc5deb09cb344231bf66a3f7ad59df8fd36
VIVEK RAJ <Vraj@vivekrajsrivastwa.onmicrosoft.com>
2020-07-02 19:29:31 +0530

    Initial commit

---
+++ MSB_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSB_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MS build");
+        }
+    }
+}
